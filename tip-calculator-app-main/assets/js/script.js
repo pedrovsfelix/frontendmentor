fb2cd0cbdf7c6bb6b1998031368aa5dec8a90e0e
Pedro Félix <pedrovitorsf@gmail.com>
2024-08-28 16:02:01 -0300

    feat: logic structure

--- tip-calculator-app-main/assets/js/script.js
+++ tip-calculator-app-main/assets/js/script.js
@@ -1,3 +1,9 @@
+const control = document.getElementById('control');
+const amount = document.getElementById('amaunt');
+const tipamount = document.getElementById('tipamount');
+const people = document.getElementById('people');
+const totalAmount = document.getElementById('total');
+
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -18,3 +24,27 @@ ScrollTrigger.addEventListener("scrollStart", () => {
   ScrollTrigger.refresh();
 
 });
+
+control.addEventListener("change", handleChange);
+
+function handleChange(event) {
+    const value = parseFloat(event.target.value);
+
+    bill = parseFloat(amount.value);
+    persons = parseFloat(people.value);
+    rate = (bill * value);
+    tiptotal = rate / persons;
+    totalAmt = (rate + bill) / persons;
+
+    if(people.value == "") {
+        people.classList.add('active');
+    } else {
+        people.classList.remove('active');
+        tipamt = tiptotal.toFixed(2);
+        tipamount.value = tipamt;
+
+        totalmt = totalAmt;
+        totalAmount.value = totalmt.toFixed(2);
+        console.log(totalmt)
+    }
+}
